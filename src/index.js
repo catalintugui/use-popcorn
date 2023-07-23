bb1ffbd558c43a8257093eec2c6f43b7f8b3887e
Catalin Tugui <katamaxx23@gmail.com>
2023-07-23 15:19:18 +0300

    using proptypes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import Rating from "./Rating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <Rating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>this movie is rated {movieRating}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Rating maxRating={5} />
+    <Rating
+      maxRating={"5"}
+      messages={["Terrible", "Bad", "Ok", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Rating maxRating={10} size={24} color="red" />
+    <Test />
   </React.StrictMode>
 );
